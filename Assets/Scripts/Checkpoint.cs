cf1dc631e06cadd805aa4113dff39f30932c09cc
Joker102810 <jaydenabridges9@gmail.com>
2026-06-26 13:18:04 +0100

    new levels

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,12 +4,11 @@ using System.Collections;
 
 public class Checkpoint : MonoBehaviour
 {
-
     [SerializeField] private LevelTimer timer;
 
-    private void OnCollisionEnter(UnityEngine.Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Checkpoint reached");
             timer.ReachCheckpoint();
@@ -21,6 +20,5 @@ public class Checkpoint : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
     }
-}   
+}
--- Assets/Scripts/Old PM.cs
+++ Assets/Scripts/Old PM.cs
@@ -48,7 +48,7 @@ public class OldPM : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 1f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, .5f, groundLayer);
     }
 
     private void Flip()
